9983b8c9edc67a49bfd6a9c90689d7c4b9fac5db
Ifeoluwa Oseni <ifeoseni@gmail.com>
2022-11-12 16:32:15 +0100

    Updated Design

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!-- resources/views/child.blade.php -->
+ 
+@extends('layouts.header')
+ 
+@section('title', 'Page Title')
+ 
+@section('sidebar')
+    @parent
+ 
+    <p>This is appended to the master sidebar.</p>
+@endsection
+ 
+@section('content')
+    <p class="text-dark">This is my body content.</p>
+    
+virtual assistant in nigeria by digital help
+Virtual Assistance
+
+A virtual assistant is a person that help you achieve your goal without being physically close to you. Need something to be done that can easily be done, then get a good virtual assistant from Digital Help today.
+Before you hire us, it is best you hear what people are saying about us on Google.	
+Check Digital Help On Google
+
+@endsection
